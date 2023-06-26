91bc76e7b945abcbdbfed9fcc0409306f57da9f4
ricguer <rj.guerra@outlook.com>
2023-06-25 20:19:49 -0400

    Initial Commit
    - Install inquirer and jest packages.
    - Create questions for logo generation.

---
+++ index.js
@@ -0,0 +1,47 @@
+                                                                /* ===================== IMPORTS ====================== */
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+                                                                /* ================= GLOBAL VARIABLES ================= */
+const logoText        =  "logoText";
+const logoTextColor   =  "logoTextColor";
+const logoShape       =  "logoShape";
+const logoShapeColor  =  "logoShapeColor";
+
+const questions = [
+    {
+        type: "input",
+        message: "Type in logo text (three letter maximum):",
+        name: logoText
+    },
+    {
+        type: "input",
+        message: "Type in desired color to logo text (hex number or color keyword):",
+        name: logoTextColor
+    },
+    {
+        type: "list",
+        message: "Select a shape for the logo:",
+        name: logoShape,
+        choices: [
+            "Circle",
+            "Triangle",
+            "Square",
+        ]
+    },
+    {
+        type: "input",
+        message: "Type in desired color for logo shape (hex number of color keyword)",
+        name: logoShapeColor
+    }
+];
+
+
+                                                                /* ================= GLOBAL FUNCTIONS ================= */
+function init() {
+                                                                /* Inquirer and collect user responses to add to README */
+    inquirer
+    .prompt(questions);
+    // TODO: Use response to generate logos.
+}
